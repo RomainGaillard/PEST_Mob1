2b557f3b03c918557825b16dfb88643e94ceefbe
RomainGaillard <romain.gaillard@y-nov.com>
2015-12-18 17:05:33 +0100

    Model Panne + Correction Config Co BDD.

---
+++ api/controllers/PanneController.js
@@ -0,0 +1,11 @@
+/**
+ * PanneController
+ *
+ * @description :: Server-side logic for managing Pannes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+

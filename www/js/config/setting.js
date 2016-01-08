85c76ce5bcad0812ad0b1128fa618685199665ae
Dorian C <Dorian.caup@y-nov.com>
2016-01-08 12:17:54 +0100

    feat(Provider): Restangular + userprovider.js

---
+++ www/js/config/setting.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Caup Dorian on 08/01/2016.
+ */
+(function(){
+  angular
+    .module('app')
+    .constant('SETTINGS', {
+      BASE_API_URL: 'http://localhost:8100/api'
+    })
+})();
---
+++ www/js/providers/userProvider.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Caup Dorian on 08/01/2016.
+ */
+/**
+ * Created by Caup Dorian on 13/11/2015.
+ */
+(function(){
+  'use strict';
+  angular
+    .module('app')
+    .factory('UserProvider', UserProvider);
+
+  OrganizationProvider.$inject = [
+    'Restangular',
+    'SETTINGS'
+  ];
+
+  function UserProvider(Restangular, SETTINGS) {
+    var provider = Restangular.setBaseUrl(SETTINGS.BASE_API_URL);
+    return {
+      'create': create,
+      'remove': remove,
+      'update': update,
+      'getAll': getAll,
+      'getOne': getOne
+    };
+
+    function create(user) {
+      return provider.one('user').customPOST(user);
+    }
+
+    function remove(idUser) {
+      return provider.one('user', idUser).remove();
+    }
+
+    function update(idUser, user) {
+      return provider.one('user', idUser).customPOST(user);
+    }
+
+    function getAll() {
+      return provider.one('user').getList();
+    }
+
+    function getOne(){
+      return provider.one('user', idUser).get();
+    }
+  }
+})();
